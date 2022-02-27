27f40fbdafae30f904ab458e9ae234595725681e
mom <josh@mediaovermatter.com>
2022-02-27 13:52:34 -0500

    initial commit

---
+++ pages/dev/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Dev {{ component }}</h1>
+    <component :is="component" v-bind="props"></component>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    component() {
+      return this.$route.query.component; // "nuxt-logo";
+    },
+    props() {
+      return JSON.parse(decodeURIComponent(this.$route.query.props)); // { title: "Title!!!" };
+    },
+  },
+};
+</script>
